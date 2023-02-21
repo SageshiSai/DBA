b1fd6c2bf5efb90ff7d10748c132771d29d6af51
SageshiSai <saikonogameryt@gmail.com>
2023-02-21 23:20:49 +0100

    Pruebavaya

---
+++ Examen/prueba_examen.sql
@@ -0,0 +1,40 @@
+/*Devuelve el id del profesor de la asignatura 'Calulo' del alumno con el nif = '17105885A'*/
+
+/*Devuelve el id, nombre, creditos, tipos y curso de las asignaturas que esta matriculado el alumno inma Lakin*/
+
+/*Devuelve la cantidad de alumnos y profesores que hay en la universidad, añadiendole un alias a la cantidad(Cantidad)*/
+
+/*Devuelve un listado con todos los alumnos que se han matriculado en alguna asignatura durante el curso 2018/2019. Agrupados por id*/
+
+/*Devuelve el numero total de alumnas que hay*/
+
+/*Devuelve el nombte de las asignaturas que contenga programacion*/
+
+/*Devuelve distintos tipos de asignaturas*/
+
+/*Delvuelve los profesores que nacieron en el año 1977*/
+
+/*Devuelve los profesores que nacieron en el año 1977 y 1988*/
+
+/*Lista el nombre y los apellidos de los alumnos en una misma tabla*/
+
+/*Lista el nombre de todos los profesores y en otra las 2 iniciales en mayusculas*/
+
+/*Lista las asignaturas de el 2do quatrimestre con el nombre en ascendente y el curso en desc*/
+
+/*Lista el nombre de los profesores que no imparten ninguna asignatura*/
+
+/*Devuelve un listado que muestre cuantos alumnos se han matriculado de alguna asignatura en cada una de los cursos
+*/
+
+/**/
+
+/**/
+
+/**/
+
+/**/
+
+/**/
+
+/**/
